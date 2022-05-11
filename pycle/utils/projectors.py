ec3a5883d524cc4ef3b40b58000016b89aaf5719
Luc Giffon <luc.giffon@ens-lyon.fr>
2022-05-11 14:30:31 +0200

    Add documentation to the projectors module.

--- pycle/utils/projectors.py
+++ pycle/utils/projectors.py
@@ -1,4 +1,16 @@
+"""
+This module contains projector functions.
+
+These projector function are used in CLOMP_CKM, at the end of each iteration,
+to let the parameters satisify some constraints. For instance, if the parameters
+are wanted to have norm = 1, there is a projector just to do that.
+
+Any custom projector can be implemented from the `Projector` base abstract class.
+"""
+
 from abc import ABCMeta, abstractmethod
+from typing import NoReturn
+
 import torch
 
 
@@ -7,7 +19,20 @@ class Projector(metaclass=ABCMeta):
     Base, asbtract, class for projectors which only need to implement the `projet` method.
     """
     @abstractmethod
-    def project(self, param):
+    def project(self, param) -> NoReturn:
+        """
+        This function will take the parameters as input and make the projection on it.
+
+        The projection must happen **in place**. It means that the `project` method doesn't
+        return anything but instead modify the object identified by `param` directly.
+
+        Parameters
+        ----------
+        param:
+            The parameters to project.
+
+
+        """
         raise NotImplementedError
 
 
